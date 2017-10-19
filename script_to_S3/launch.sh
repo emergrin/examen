db8b9268abb394ef4dc6dc8f64b7481b302f6101
eduardo.gimenez <eduardo.gimenez@bbva.com>
2017-10-19 10:11:59 +0200

    update

--- script_to_S3/launch.sh
+++ script_to_S3/launch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-option=$OP
-
 function err_ {
 if [ $? != 0 ]; then
       echo "KO: error on $1"
@@ -10,13 +8,14 @@ else
       echo "OK: step $1"
 fi
 }
-echo uno
+echo "Empezamos"
 function inicial {
 # configure AWS
+mkdir ~/.aws
 echo "[default]" >> ~/.aws/config
 echo "region = eu-west-1" >> ~/.aws/config
-echo "aws_secret_access_key = $1"  >> ~/.aws/config
-echo "aws_access_key_id = $2" >> ~/.aws/config
+echo "aws_secret_access_key = $VAR_ACC"  >> ~/.aws/config
+echo "aws_access_key_id = $VAR_KEY" >> ~/.aws/config
 
 aws s3 ls
 err_ "aws cli configure"
@@ -33,23 +32,26 @@ echo "terraform plan -out ./tf.plan.output"
 err_ "terraform plan"
 echo "terraform apply ./tf.apply.output"
 err_ "terraform init"
-
 }
-
 
 function carga {
   # change files on S3
-  curl -o files/$1.json http://www.omdbapi.com/?apikey=3e721637&t=$1
+  curl -o files/$1.json http://www.omdbapi.com/?apikey=$VAR_API&t=$1
+  aws s3 cp /files/$1.json s3://$bucket/
 }
 
-case $option in
+case $VAR_OP in
   inicial)
-       inicial $aws_access $aws_secret
+       inicial
   ;;
   carga)
-       carga
+      for i in $(cat ./$lt2 )
+      do
+        carga $1
+      done
   ;;
   *)
-       exit
+      echo "No está cargada la variable VAR_OP"
+      exit
   ;;
 esac
